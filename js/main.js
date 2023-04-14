aa74a80c1b1fb7de172394264e0644dcf99f1a44
ssuk80 <xkftjs@gmail.com>
2023-04-14 15:37:28 +0900

    sns

--- js/main.js
+++ js/main.js
@@ -19,3 +19,38 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', ''); // HTML 속성을 추가하는 메소드
 });
+
+
+
+// //스크롤 시 전역 배지(고정배너)숨기기
+// const badgeEl = document.querySelector('header .badges');
+
+// //window:브라우저 창 객체
+// window.addEventListener('scroll', function () {
+//   // console.log(window.scrollY);
+//   //  y축으로 얼마나 스크롤 됐는지 대한 수치
+//   //  만약 스크롤의 위치가 500을 넘어가면 배지 요소를 숨기고 
+//   //  그렇지않으면 다시 보이기!
+  
+//   if (window.scrollY > 500 ) {
+//     // 배지요소 숨김
+//     badgeEl.style.display = 'none';
+//   } else {
+//     // 배지요소 보이기  
+//     badgeEl.style.display = 'block';
+// }
+
+// });
+
+
+
+  
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener ('scroll', function() {
+ if ( window.scrollY < 500 )
+ {  badgeEl.style.display = 'block' } 
+ else {  badgeEl.style.display = 'none'
+ }
+
+});
